fabc6bc23b92074984e83abbe8f265bcbcd265ff
Matt Kenney <matt.kenney@ravenhealth.com>
2025-11-06 16:53:53 -0500

    update android vars; wezterm cmd-k

--- files/.config/wezterm/wezterm.lua
+++ files/.config/wezterm/wezterm.lua
@@ -12,5 +12,13 @@ config.font_size = 14.0
 config.hide_tab_bar_if_only_one_tab = true
 config.selection_word_boundary = '\t\n !"#%&\'()*+,-./:;<=>?@[\\]^`{|}~'
 
+config.keys = {
+  {
+    action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
+    key = 'k',
+    mods = 'CMD',
+  },
+}
+
 -- Finally, return the configuration to wezterm:
 return config
--- files/.oh-my-zsh/custom/variables.zsh
+++ files/.oh-my-zsh/custom/variables.zsh
@@ -1,6 +1,6 @@
 export CLICOLOR=1
 export EDITOR=nvim
-export PATH="$PATH:/usr/local/go/bin:$HOME/bin"
+PATH="$PATH:/usr/local/go/bin:$HOME/bin"
 
 # Java + Android Studio
 if [ -e /usr/libexec/java_home ]; then
@@ -8,9 +8,10 @@ if [ -e /usr/libexec/java_home ]; then
 else
     export JAVA_HOME=/usr/lib/jvm/default-java
 fi
-if [ -d ~/Library/Android/sdk ]; then
-  ANDROID_SDK_ROOT=~/Library/Android/sdk
-fi
+export ANDROID_HOME=~/Library/Android/sdk
+export ANDROID_AVD_HOME=~/.android/avd
+export ANDROID_SDK_ROOT=$ANDROID_HOME
+PATH=$PATH:$ANDROID_HOME/platform-tools
 
 # Ruby
 if command -v brew >/dev/null 2>&1; then
@@ -18,7 +19,7 @@ if command -v brew >/dev/null 2>&1; then
     PATH="${HOMEBREW_PREFIX}/opt/ruby/bin:$PATH"
 fi
 export GEM_HOME=$HOME/.gem
-export PATH=$GEM_HOME/bin:$PATH
+PATH=$GEM_HOME/bin:$PATH
 
 # add with `security add-generic-password -a "$USER" -s "NPM_TOKEN" -w "something"`
 export NPM_TOKEN=$(security find-generic-password -a "$USER" -s "NPM_TOKEN" -w 2>/dev/null)
