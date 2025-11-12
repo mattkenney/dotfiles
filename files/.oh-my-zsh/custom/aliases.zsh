1547351bb5e81ca0281042e3f866cdbd2fa06694
Matt Kenney <matt.kenney@ravenhealth.com>
2025-11-12 18:55:25 -0500

    alias adjustments

--- files/.oh-my-zsh/custom/aliases.zsh
+++ files/.oh-my-zsh/custom/aliases.zsh
@@ -12,10 +12,11 @@ alias kc=kubectl
 alias kj='kubectl --output json'
 alias kk='$(pickube)'
 alias mvx='mv -i'
-alias n=npmx
+alias n=pkg-mgr
 alias na='npm run --if-present lint && npm run --if-present format && npm run --if-present build'
 alias ng='npx --package @angular/cli ng'
-alias npm=npmx
+alias npm=pkg-mgr
+alias npmx='whence -p npm && $(whence -p npm)'
 alias nr='npm run'
 alias nsx='lsof -P -iTCP -n -sTCP:LISTEN'
 alias po='pino-pretty --colorize --translateTime "UTC:yyyy-mm-dd'"'"'T'"'"'HH:MM:ss'"'"'Z'"'"'"|less -R;reset'
@@ -25,7 +26,8 @@ alias sm='alacritty --option font.size=10 --option window.dimensions.lines=80 &'
 alias v=nvim
 alias vim=nvim
 alias vimdiff='nvim -d'
-alias yarn=npmx
+alias yarn=pkg-mgr
+alias yarnx='whence -p yarn && $(whence -p yarn)'
 
 if [ $(uname) = Darwin ]; then
     alias ssh='ssh -o "XAuthLocation=/opt/X11/bin/xauth"'
