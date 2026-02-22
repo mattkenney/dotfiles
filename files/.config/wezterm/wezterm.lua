711f87c699767987baea687f4e4292ba525fb245
Matt Kenney <mkenney@mjk-tech.com>
2026-02-22 12:00:54 -0500

    wezterm tab appearance

--- files/.config/wezterm/wezterm.lua
+++ files/.config/wezterm/wezterm.lua
@@ -12,6 +12,7 @@ config.font_size = 14.0
 config.hide_tab_bar_if_only_one_tab = true
 config.selection_word_boundary = '\t\n !"#%&\'()*+,-./:;<=>?@[\\]^`{|}~'
 
+-- custom key bindings
 config.keys = {
   {
     action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
@@ -19,6 +20,19 @@ config.keys = {
     mods = 'CMD',
   },
 }
+
+config.window_frame = {
+  font = wezterm.font("Andale Mono", { weight = "Bold" }),
+  font_size = 14.0,
+}
+
+-- make tab titles easier to read by adding padding
+wezterm.on("format-tab-title", function(tab)
+  local title = " " .. (tab.tab_index + 1) .. ": " .. tab.active_pane.title
+  return {
+    { Text = wezterm.pad_right(title, 12) },
+  }
+end)
 
 -- Finally, return the configuration to wezterm:
 return config
