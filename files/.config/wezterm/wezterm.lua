8920648877e5b3600819460265e257541c93ed5b
Matt Kenney <mkenney@mjk-tech.com>
2026-08-22 20:38:35 -0400

    adjust wezterm titlebar on linux

--- files/.config/wezterm/wezterm.lua
+++ files/.config/wezterm/wezterm.lua
@@ -4,15 +4,26 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
-local is_mac = wezterm.target_triple:find 'darwin' ~= nil
-
 -- custom config
 config.color_scheme = 'Tomorrow (Gogh)'
 config.cursor_thickness = 3
 config.font = wezterm.font 'Andale Mono'
-config.font_size = is_mac and 14.0 or 11.0
+config.font_size = 14.0
 config.hide_tab_bar_if_only_one_tab = true
 config.selection_word_boundary = '\t\n !"#%&\'()*+,-./:;<=>?@[\\]^`{|}~'
+
+config.keys = {
+  {
+    action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
+    key = 'k',
+    mods = 'CMD',
+  },
+}
+
+config.window_frame = {
+  font = wezterm.font("Andale Mono", { weight = "Bold" }),
+  font_size = 14.0,
+}
 
 -- use wezterm's terminfo if it's installed locally
 local function has_terminfo(name)
@@ -24,33 +35,6 @@ if has_terminfo('wezterm') then
   config.term = 'wezterm'
 end
 
--- custom key bindings
-config.keys = {
-  {
-    action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
-    key = 'k',
-    mods = 'CMD',
-  },
-}
-
-config.window_frame = {
-  font = wezterm.font("Andale Mono", { weight = "Bold" }),
-  font_size = is_mac and 14.0 or 11.0,
-}
-
--- macos already draws its own window border
-if not is_mac then
-  local border_color = '#484848'
-  local border_size = '1px'
-
-  config.window_frame.border_bottom_color = border_color
-  config.window_frame.border_bottom_height = border_size
-  config.window_frame.border_left_color = border_color
-  config.window_frame.border_left_width = border_size
-  config.window_frame.border_right_color = border_color
-  config.window_frame.border_right_width = border_size
-end
-
 -- make tab titles easier to read by adding padding
 wezterm.on("format-tab-title", function(tab)
   local title = " " .. (tab.tab_index + 1) .. ": " .. tab.active_pane.title
@@ -58,6 +42,25 @@ wezterm.on("format-tab-title", function(tab)
     { Text = wezterm.pad_right(title, 20) },
   }
 end)
+
+local is_mac = wezterm.target_triple:find 'darwin' ~= nil
+if not is_mac then
+  local border_color = '#484848'
+  local border_size = '1px'
+
+  config.font_size = 11.0
+  config.front_end = 'OpenGL'
+  config.hide_tab_bar_if_only_one_tab = false
+  config.integrated_title_button_style = 'Gnome'
+  config.window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
+  config.window_frame.border_bottom_color = border_color
+  config.window_frame.border_bottom_height = border_size
+  config.window_frame.border_left_color = border_color
+  config.window_frame.border_left_width = border_size
+  config.window_frame.border_right_color = border_color
+  config.window_frame.border_right_width = border_size
+  config.window_frame.font_size = 11.0
+end
 
 -- Finally, return the configuration to wezterm:
 return config
